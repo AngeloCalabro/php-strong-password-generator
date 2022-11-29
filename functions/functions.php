3d7911639f875534f49a4084d8d7d3dcadb920d6
AngeloCalabro <angelo.1991@hotmail.com>
2022-11-29 17:16:47 +0100

    psw generate

--- functions/functions.php
+++ functions/functions.php
@@ -1,5 +1,9 @@
 <?php
-
-
+    
+function getChar($string): string{
+    $length= strlen($string) - 1;
+    $randomNumber = rand(0,$length);
+    return $string[$randomNumber];
+}; 
 
 ?>
--- index.php
+++ index.php
@@ -1,9 +1,30 @@
 <?php
 include __DIR__ .'/functions/functions.php';
 
+// $errorClass = "is-invalid";
+$alfabeto = 'abcdefghijklmnopqrstuvwxyz';
+$upAlfabeto = strtoupper($alfabeto);
+$numeri = '0123456789';
+$simboli = '[]()/*#!%&?;@-_.';
+$arrayTot = $alfabeto.$upAlfabeto.$numeri.$simboli; 
+$arrayTot = str_shuffle($arrayTot); 
+
+$password = '';
+
+if(isset($_GET['numberPsw']) && !empty($_GET['numberPsw'])){
+    // var_dump($_GET['numberPsw']);
+    $numberPsw = $_GET['numberPsw'];
+    while(strlen($password) < $numberPsw){
+        $password .= getChar($arrayTot);
+        $result = $password;
+    };
+    $password = str_shuffle($password);
+        var_dump($password);
+
+}
+
 include __DIR__ .'/partials/header.php';
 ?>
-
 
 <body class="bg-primary">
     <header class="container m-auto pb-3">
@@ -11,15 +32,31 @@ include __DIR__ .'/partials/header.php';
         <h2 class="text-white text-center">Genera una password sicura</h2>
     </header>
     <main class="container m-auto">
-        <section></section>
+        <section>
+            <!-- SEZIONE PER MESSAGGIO -->
+            <?php  
+    if(isset($result) && $result){
+        if($result){ ?>
+            <div class="alert alert-success text-center">
+                <h3 class="text-center">Password generata: <b><?php echo $password ?></b></h3>
+                <p>La tua password ha <?php echo strlen($password) ?> caratteri</p>
+            </div>
+            <?php } else { ?>
+            <div class="alert alert-danger">
+                Errore <br />
+                Formato non corretto
+            </div>
+            <?php }} ?>
+        </section>
         <section class="bg-white rounded-5 p-5">
-            <form action="index.php" method="POST">
+            <form action="index.php" method="GET" name="numbGenForm">
                 <div class="row g-0">
                     <div class="col-6 my-2">
                         <div>Lunghezza password:</div>
                     </div>
                     <div class="col-6 my-2">
-                        <input type="number" class="w-100" required>
+                        <input type="number" class="w-100" id="numberPsw" name="numberPsw" value="10" min="10" max="20"
+                            required>
                     </div>
                     <div class="col-6 my-2">
                         <div>Consenti ripetizioni di uno o più caratteri:</div>
